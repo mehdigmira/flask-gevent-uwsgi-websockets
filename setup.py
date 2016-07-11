7ddb9d88a9e7d4f06a9dcb31a406ff928379e8ec
mehdi gmira <mgmira@wiremind.fr>
2016-07-11 17:05:08 +0200

    small changes

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ Flask-Gevent-uWSGI-Websockets
 
 This is the description for that library
 """
-from setuptools import setup
 
+from setuptools import setup, find_packages
 
 setup(
     name='Flask-Gevent-uWSGI-Websockets',
     version='0.1.1',
-    url='',
+    url='https://github.com/mehdigmira/flask-gevent-uwsgi-websockets',
     license='MIT',
     author='Mehdi GMIRA',
     author_email='mehdigmira@gmail.com',
@@ -18,9 +18,7 @@ setup(
                 'The library is designed so that only one websocket connection is opened per browser session.',
     long_description=__doc__,
     py_modules=['flask_gevent_uwsgi_websockets'],
-    # if you would be using a package instead use packages instead
-    # of py_modules:
-    # packages=['flask_sqlite3'],
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
